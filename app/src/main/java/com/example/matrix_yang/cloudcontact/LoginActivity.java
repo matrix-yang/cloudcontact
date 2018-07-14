e8a51ae0a9c4f3039d4b44b24d5ba2e82b3d2c37
matrix-yang <739260804@qq.com>
2018-07-14 18:01:05 +0800

    调整样式 绑定事件

--- app/src/main/java/com/example/matrix_yang/cloudcontact/LoginActivity.java
+++ app/src/main/java/com/example/matrix_yang/cloudcontact/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.matrix_yang.cloudcontact;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import util.EditTextClearTools;
 
 public class LoginActivity extends AppCompatActivity {
@@ -21,8 +24,28 @@ public class LoginActivity extends AppCompatActivity {
         ImageView unameClear = (ImageView) findViewById(R.id.iv_unameClear);
         ImageView pwdClear = (ImageView) findViewById(R.id.iv_pwdClear);
 
+
         EditTextClearTools.addClearListener(userName,unameClear);
         EditTextClearTools.addClearListener(password,pwdClear);
+
+        TextView reg= (TextView) findViewById(R.id.textView11);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(LoginActivity.this,editActivity.class);
+                intent.putExtra("info","20,姓名,电话号码,性别,学号,密码");
+                startActivity(intent);
+            }
+        });
+
+        Button button= (Button) findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
