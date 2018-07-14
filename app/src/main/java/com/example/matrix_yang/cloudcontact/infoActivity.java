6ac2fc0a37c800a0c26928e7736311e864d4be10
matrix-yang <739260804@qq.com>
2018-07-14 15:55:16 +0800

    complete the design of info

--- app/src/main/java/com/example/matrix_yang/cloudcontact/infoActivity.java
+++ app/src/main/java/com/example/matrix_yang/cloudcontact/infoActivity.java
@@ -3,6 +3,7 @@ package com.example.matrix_yang.cloudcontact;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import model.Friend;
 import util.Util;
 
@@ -15,6 +16,14 @@ public class infoActivity extends AppCompatActivity {
         Intent intent=getIntent();
         String s=intent.getStringExtra("info");
         Friend friend= Util.string2Friend(s);
-        System.out.println("222222222222222222"+friend.toString());
+        //System.out.println("222222222222222222"+friend.toString());
+        TextView name = (TextView) findViewById(R.id.textView2);
+        name.setText(friend.getName());
+        TextView phone = (TextView) findViewById(R.id.textView4);
+        phone.setText(friend.getPhoneNum());
+        TextView sex = (TextView) findViewById(R.id.textView6);
+        sex.setText(friend.getSex());
+        TextView stuid = (TextView) findViewById(R.id.textView8);
+        stuid.setText(friend.getStuId());
     }
 }
